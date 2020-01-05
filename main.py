7057be9b38ded02aa74809bd9a5505687b313ecb
dej-e <developde@mail.ru>
2020-01-05 23:13:39 +0300

    Replace main.py to manage.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-from django.core.management import execute_from_command_line
-
-if __name__ == "__main__":
-    load_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-    execute_from_command_line("manage.py runserver 0.0.0.0:8000".split())
